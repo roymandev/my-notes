1153142567c1cee50d493c1fc7ac5b46db9049f9
Royman <royman.dev@gmail.com>
2022-07-18 03:04:24 +0700

    feat: add NoteListItem component

---
+++ src/components/NoteListItem.tsx
@@ -0,0 +1,32 @@
+import BaseButton from '@/components/BaseButton';
+import { atomNotesSelected } from '@/stores/notesStore';
+import { Note } from '@/types/noteTypes';
+import { useSetAtom } from 'jotai';
+import { twMerge } from 'tailwind-merge';
+
+interface NoteListItemProps {
+  note: Note;
+  isSelected: boolean;
+}
+
+const NoteListItem = ({ note, isSelected }: NoteListItemProps) => {
+  const selectNote = useSetAtom(atomNotesSelected);
+
+  const selectNoteHandler = () => selectNote(note);
+
+  return (
+    <li>
+      <BaseButton
+        className={twMerge(
+          'w-full px-3 py-2 text-left',
+          isSelected && 'bg-slate-700 text-slate-300',
+        )}
+        onClick={selectNoteHandler}
+      >
+        {note.title || '(Untitled)'}
+      </BaseButton>
+    </li>
+  );
+};
+
+export default NoteListItem;
