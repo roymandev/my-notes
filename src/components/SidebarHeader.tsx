97f0690f31f4777dc1602a9e601eddfe968d68f2
Royman <royman.dev@gmail.com>
2022-07-18 02:35:56 +0700

    feat: add SidebarHeader component

---
+++ src/components/SidebarHeader.tsx
@@ -0,0 +1,20 @@
+import BaseButton from '@/components/BaseButton';
+import { atomNotesAdd } from '@/stores/notesStore';
+import { useSetAtom } from 'jotai';
+import { RiAddFill } from 'react-icons/ri';
+
+const SidebarHeader = () => {
+  const addNewNote = useSetAtom(atomNotesAdd);
+
+  return (
+    <header className="flex items-center border-b border-slate-700 p-1">
+      <h1 className="px-2 py-1 text-xl font-bold">My Notes</h1>
+
+      <BaseButton className="ml-auto p-2" onClick={addNewNote}>
+        <RiAddFill className="h-5 w-5" />
+      </BaseButton>
+    </header>
+  );
+};
+
+export default SidebarHeader;
