757974310cdd10978b8b7d854c5f9df4b5c6ba9a
Royman <royman.dev@gmail.com>
2022-07-18 03:03:38 +0700

    feat(stores): noteStore implement selected note state

--- src/stores/notesStore.ts
+++ src/stores/notesStore.ts
@@ -4,8 +4,23 @@ import { atomWithStorage } from 'jotai/utils';
 
 // State
 export const atomNotes = atomWithStorage<Note[]>('notes', []);
+export const atomNotesSelected = atom<Note | null>(null);
 
 // Actions
+
+export const atomNotesSelectedWrite = atom(
+  null,
+  (get, set, updated: Note | null) => {
+    set(atomNotesSelected, updated);
+
+    if (updated)
+      set(
+        atomNotes,
+        get(atomNotes).map((note) => (note.id === updated.id ? updated : note)),
+      );
+  },
+);
+
 export const atomNotesAdd = atom(null, (get, set) => {
   const notes = get(atomNotes);
   const newNote: Note = {
