210ca5a78082dbc6fe8f8c672385ee13db0350ec
Royman <royman.dev@gmail.com>
2022-07-18 03:04:37 +0700

    feat: add NoteList component

---
+++ src/components/NoteList.tsx
@@ -0,0 +1,22 @@
+import NoteListItem from '@/components/NoteListItem';
+import { atomNotes, atomNotesSelected } from '@/stores/notesStore';
+import { useAtomValue } from 'jotai';
+
+const NoteList = () => {
+  const notes = useAtomValue(atomNotes);
+  const selectedNotes = useAtomValue(atomNotesSelected);
+
+  return (
+    <ul className="flex flex-1 flex-col gap-1 overflow-y-auto p-1">
+      {notes.map((note) => (
+        <NoteListItem
+          key={note.id}
+          note={note}
+          isSelected={note.id === selectedNotes?.id}
+        />
+      ))}
+    </ul>
+  );
+};
+
+export default NoteList;
--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,9 +1,12 @@
+import NoteList from '@/components/NoteList';
 import SidebarHeader from '@/components/SidebarHeader';
 
 const TheSidebar = () => {
   return (
     <aside className="w-[320px] border-r border-slate-700">
       <SidebarHeader />
+
+      <NoteList />
     </aside>
   );
 };
