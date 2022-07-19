68576f282961c81a9a606e4e19d31a713239a319
Royman <royman.dev@gmail.com>
2022-07-19 20:23:31 +0700

    feat(pages): add Login page

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import { Route, Routes } from 'react-router-dom';
 import NoteApp from '@/pages/NoteApp';
+import Login from '@/pages/Login';
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<NoteApp />} />
+      <Route path="/login" element={<Login />} />
     </Routes>
   );
 }
---
+++ src/pages/Login.tsx
@@ -0,0 +1,34 @@
+import BaseButton from '@/components/BaseButton';
+import BaseInput from '@/components/BaseInput';
+
+const Login = () => {
+  return (
+    <main className="h-screen bg-slate-900 py-20 px-4 text-slate-300">
+      <h1 className="px-4 py-10 text-center text-6xl font-bold">My Notes</h1>
+      <section className="mx-auto max-w-md rounded bg-slate-800 p-4 shadow">
+        <h2 className="p-4 text-center text-2xl font-medium">Login</h2>
+        <form className="flex flex-col gap-4">
+          <fieldset className="flex flex-col gap-1">
+            <label htmlFor="email" className="text-lg font-medium">
+              Email
+            </label>
+            <BaseInput type="email" id="email" name="email" />
+          </fieldset>
+
+          <fieldset className="flex flex-col gap-1">
+            <label htmlFor="password" className="text-lg font-medium">
+              Password
+            </label>
+            <BaseInput type="password" id="password" name="password" />
+          </fieldset>
+
+          <BaseButton type="submit" variant="primary" className="p-2 text-lg">
+            Login
+          </BaseButton>
+        </form>
+      </section>
+    </main>
+  );
+};
+
+export default Login;
