201f509d5a326f78db14d21b8febb007c00c8dbf
Royman <royman.dev@gmail.com>
2022-07-20 03:29:42 +0700

    feat: add LoadingScreen component

---
+++ src/components/LoadingScreen.tsx
@@ -0,0 +1,9 @@
+const LoadingScreen = () => {
+  return (
+    <div className="fixed inset-0 flex items-center justify-center bg-slate-800 p-4 text-slate-300">
+      <h1 className="text-6xl font-bold">Loading...</h1>
+    </div>
+  );
+};
+
+export default LoadingScreen;
