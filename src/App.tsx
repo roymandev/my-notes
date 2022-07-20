607ed36f4290d22f6ea2b3dbecae0822440577bd
Royman <royman.dev@gmail.com>
2022-07-20 21:55:38 +0700

    refactor(services): remove firebase

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,25 @@ import { Route, Routes } from 'react-router-dom';
 import NoteApp from '@/pages/NoteApp';
 import Login from '@/pages/Login';
 import AuthRoute from '@/components/AuthRoute';
+import { firebaseConfig } from '@/../firebase.config';
+import { initializeApp } from 'firebase/app';
+import { getFirestore } from 'firebase/firestore';
+import { getAuth, GoogleAuthProvider, onAuthStateChanged } from 'firebase/auth';
+import { useSetAtom } from 'jotai';
+import { atomUser } from '@/stores/userStore';
 
-import '@/services/firebase';
+export const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
+export const auth = getAuth(app);
+export const oAuthProvider = new GoogleAuthProvider();
 
 function App() {
+  const setUser = useSetAtom(atomUser);
+
+  onAuthStateChanged(auth, (user) => {
+    setUser(user);
+  });
+
   return (
     <Routes>
       <Route
--- src/hooks/useAuth.tsx
+++ src/hooks/useAuth.tsx
@@ -1,16 +1,12 @@
-import { auth, oAuthProvider } from '@/services/firebase';
-import { atomUser } from '@/stores/userStore';
+import { auth, oAuthProvider } from '@/App';
 import { signInWithPopup, signOut } from 'firebase/auth';
-import { useSetAtom } from 'jotai';
 import { useNavigate } from 'react-router-dom';
 
 const useAuth = () => {
-  const setUser = useSetAtom(atomUser);
   const navigate = useNavigate();
 
   const logIn = () =>
     signInWithPopup(auth, oAuthProvider).then((result) => {
-      setUser(result.user);
       navigate('/');
       return result;
     });
@@ -18,7 +14,6 @@ const useAuth = () => {
   const logOut = () =>
     signOut(auth).then(() => {
       {
-        setUser(null);
         navigate('/login');
       }
     });
