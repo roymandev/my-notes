0884f5a9554026324f8e06134bba681f99e52c41
Royman <royman.dev@gmail.com>
2022-07-21 07:20:09 +0700

    refactor: move notes fetch to AuthRoute component

--- src/App.tsx
+++ src/App.tsx
@@ -27,7 +27,7 @@ function App() {
       <Route
         path="/"
         element={
-          <AuthRoute>
+          <AuthRoute fetchNotes>
             <NoteApp />
           </AuthRoute>
         }
