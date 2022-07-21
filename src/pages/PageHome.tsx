6647291f5bf784cb192feb64a4715076c2162e4f
Royman <royman.dev@gmail.com>
2022-07-21 08:13:24 +0700

    feat: add listenBackButton event listener

--- src/pages/PageHome.tsx
+++ src/pages/PageHome.tsx
@@ -1,7 +1,19 @@
 import NoteViewer from '@/components/NoteViewer';
 import Sidebar from '@/components/Sidebar';
+import { atomNotesSelected } from '@/stores/notesStore';
+import { useSetAtom } from 'jotai';
+import { useEffect } from 'react';
 
 const PageHome = () => {
+  const setSelectedNote = useSetAtom(atomNotesSelected);
+
+  useEffect(() => {
+    const listenBackButton = window.addEventListener('popstate', () =>
+      setSelectedNote((current) => current && null),
+    );
+    return listenBackButton;
+  }, []);
+
   return (
     <main className="fixed inset-0 divide-x divide-slate-700 bg-slate-800 text-lg text-slate-300 md:flex">
       <Sidebar />
