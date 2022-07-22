68a18511463751006330ef6d91de005b65b7da8c
Royman <royman.dev@gmail.com>
2022-07-23 05:36:12 +0700

    feat(services): add firestore getUserNotes

--- src/services/firebase.ts
+++ src/services/firebase.ts
@@ -1,18 +1,52 @@
 import { firebaseConfig } from '@/config/firebase';
 import { initializeApp } from 'firebase/app';
-import { getFirestore } from 'firebase/firestore';
+import {
+  collection,
+  CollectionReference,
+  getDocs,
+  getFirestore,
+  query,
+  where,
+} from 'firebase/firestore';
 import {
   getAuth,
   GoogleAuthProvider,
   signInWithPopup,
   signOut,
+  User,
 } from 'firebase/auth';
+import { Note } from '@/types/noteTypes';
 
 export const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
 export const auth = getAuth(app);
 export const oAuthProvider = new GoogleAuthProvider();
 
+// Authentication
 export const login = () => signInWithPopup(auth, oAuthProvider);
-
 export const logout = () => signOut(auth);
+
+// Firestore
+const notesRef = collection(db, 'notes') as CollectionReference<Note>;
+const whereIsOwner = (user: User) => where('uid', '==', user.uid);
+
+export const getUserNotes = async (user: User) => {
+  try {
+    const q = query(notesRef, whereIsOwner(user));
+    const querySnapshot = await getDocs(q);
+
+    const result: Note[] = [];
+
+    querySnapshot.forEach((note) => {
+      result.push({
+        ...note.data(),
+      } as Note);
+    });
+
+    console.info('Success get user notes');
+
+    return result;
+  } catch (error) {
+    console.error('Firebase Error, Failed to get user notes.');
+  }
+};
