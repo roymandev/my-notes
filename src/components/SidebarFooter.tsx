576d107b79483691d90f5f4f5163945dd4cbdde5
Royman <royman.dev@gmail.com>
2022-07-22 07:28:34 +0700

    refactor: SidebarFooter change logout method

--- src/components/SidebarFooter.tsx
+++ src/components/SidebarFooter.tsx
@@ -1,18 +1,17 @@
 import BaseButton from '@/components/BaseButton';
-import useAuth from '@/hooks/useAuth';
+import { logout } from '@/services/firebase';
 import { atomUser } from '@/stores/userStore';
 import { useAtomValue } from 'jotai';
 import { RiLogoutBoxLine } from 'react-icons/ri';
 
 const SidebarFooter = () => {
-  const { logOut } = useAuth();
   const user = useAtomValue(atomUser);
 
   return (
     <footer className="flex items-center gap-4 p-2">
       {user?.photoURL && <img src={user.photoURL} className="h-10 w-10" />}
       <div>{user?.displayName}</div>
-      <BaseButton className="ml-auto p-3" onClick={logOut}>
+      <BaseButton className="ml-auto p-3" onClick={logout}>
         <RiLogoutBoxLine className="h-6 w-6" />
       </BaseButton>
     </footer>
