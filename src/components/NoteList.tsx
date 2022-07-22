f4c6ebe8307638eb73f5105090bc975525c7625e
Royman <royman.dev@gmail.com>
2022-07-23 05:54:38 +0700

    feat: NoteList use fetchNotes

--- src/components/NoteList.tsx
+++ src/components/NoteList.tsx
@@ -1,10 +1,20 @@
 import NoteListItem from '@/components/NoteListItem';
+import useUserNotes from '@/hooks/useUserNotes';
 import { atomNotesFiltered, atomNotesSelected } from '@/stores/notesStore';
 import { useAtomValue } from 'jotai';
+import { useEffect } from 'react';
 
 const NoteList = () => {
   const notes = useAtomValue(atomNotesFiltered);
   const selectedNotes = useAtomValue(atomNotesSelected);
+
+  const { fetchNotes } = useUserNotes();
+
+  useEffect(() => {
+    (async () => {
+      await fetchNotes();
+    })();
+  }, []);
 
   return (
     <ul className="flex flex-1 flex-col gap-1 overflow-y-auto p-1">
