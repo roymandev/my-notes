2643bc2abab1906c026e7a40c6d1e505db0ccb1d
Royman <royman.dev@gmail.com>
2022-07-27 20:05:10 +0700

    refactor: make NoteViewerHead stateless

--- src/components/NoteViewerHead.tsx
+++ src/components/NoteViewerHead.tsx
@@ -1,34 +1,34 @@
 import BaseButton from '@/components/UI/BaseButton';
-import { atomNotesSelected, atomNotesSelectedId } from '@/stores/notesStore';
-import { useAtomValue, useSetAtom } from 'jotai';
 import { RiArrowLeftSLine, RiDeleteBin2Line } from 'react-icons/ri';
 import { formatDate } from '@/utils/formatDate';
-import useUserNotes from '@/hooks/useUserNotes';
 
-const NoteViewerHead = () => {
-  const selectedNote = useAtomValue(atomNotesSelected);
-  const setSelectedNoteId = useSetAtom(atomNotesSelectedId);
-  const { deleteNote } = useUserNotes();
+export interface NoteViewerHeadProps {
+  updatedAt: string;
+  onDeleteNote: () => void;
+  onReturn?: () => void;
+}
 
-  const deleteNoteHandler = () => selectedNote && deleteNote(selectedNote);
-
+const NoteViewerHead = ({
+  updatedAt,
+  onDeleteNote,
+  onReturn,
+}: NoteViewerHeadProps) => {
   return (
     <header className="flex items-center p-2">
-      <BaseButton
-        className="p-2 md:hidden"
-        onClick={() => setSelectedNoteId(null)}
-      >
-        <RiArrowLeftSLine className="h-8 w-8" />
-      </BaseButton>
+      {onReturn && (
+        <BaseButton className="p-2 md:hidden" onClick={onReturn}>
+          <RiArrowLeftSLine className="h-8 w-8" />
+        </BaseButton>
+      )}
 
       <p className="flex-1 px-4 text-center text-base md:text-left md:text-lg">
         <span className="text-slate-400">Updated at:</span>{' '}
-        {selectedNote && formatDate(selectedNote.updatedAt)}
+        {formatDate(updatedAt)}
       </p>
 
       <BaseButton
         className="ml-auto p-3 text-rose-500/70 hover:text-rose-500"
-        onClick={deleteNoteHandler}
+        onClick={onDeleteNote}
       >
         <RiDeleteBin2Line className="h-6 w-6" />
       </BaseButton>
