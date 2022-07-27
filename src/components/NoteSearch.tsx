65fdd52fef88b4620a953604d4c2ae741807a3d7
Royman <royman.dev@gmail.com>
2022-07-28 04:19:13 +0700

    refactor: make NoteSearch stateless

--- src/components/NoteSearch.tsx
+++ src/components/NoteSearch.tsx
@@ -1,17 +1,19 @@
 import BaseInput from '@/components/UI/BaseInput';
-import { atomNotesSearch } from '@/stores/notesStore';
-import { useAtom } from 'jotai';
 
-const NoteSearch = () => {
-  const [search, setSearch] = useAtom(atomNotesSearch);
+export interface NoteSearchProps {
+  query: string;
+  setQuery: (newQuery: string) => void;
+}
+
+const NoteSearch = ({ query, setQuery }: NoteSearchProps) => {
   return (
     <div className="p-1">
       <BaseInput
         className="w-full"
         type="text"
         placeholder="Search notes"
-        value={search}
-        onChange={(e) => setSearch(e.target.value)}
+        value={query}
+        onChange={(e) => setQuery(e.target.value)}
       />
     </div>
   );
