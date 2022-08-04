9acb6807fde610c8f52f2379832da6a5db4fa2aa
Royman <royman.dev@gmail.com>
2022-08-05 01:07:48 +0700

    feat: implement modal delete note

---
+++ src/components/Modal/ModalDeleteNote.tsx
@@ -0,0 +1,39 @@
+import BaseModal from '@/components/Modal/BaseModal';
+import BaseButton from '@/components/UI/BaseButton';
+import useUserNotes from '@/hooks/useUserNotes';
+import { atomModalClose } from '@/stores/appStore';
+import { atomNotesSelected } from '@/stores/notesStore';
+import { useAtomValue, useSetAtom } from 'jotai';
+
+const ModalDeleteNote = () => {
+  const closeModal = useSetAtom(atomModalClose);
+  const selectedNote = useAtomValue(atomNotesSelected);
+  const { deleteNote } = useUserNotes();
+
+  return (
+    <BaseModal title="Delete note confirmation">
+      <div className="p-4">
+        <p>
+          Delete &quot;<b>{selectedNote?.title || '(Untitled)'}</b>&quot; note?
+        </p>
+      </div>
+      <div className="flex justify-end gap-2 p-2">
+        <BaseButton className="px-3 py-1" onClick={closeModal}>
+          Close
+        </BaseButton>
+        <BaseButton
+          className="px-3 py-1 text-rose-500"
+          variant="primary"
+          onClick={() => {
+            selectedNote && deleteNote(selectedNote);
+            closeModal();
+          }}
+        >
+          Delete
+        </BaseButton>
+      </div>
+    </BaseModal>
+  );
+};
+
+export default ModalDeleteNote;
--- src/pages/PageHome.tsx
+++ src/pages/PageHome.tsx
@@ -1,8 +1,9 @@
 import FallbackNoSelectedNote from '@/components/Fallback/FallbackNoSelectedNote';
+import ContainerModal from '@/components/Modal/ContainerModal';
 import NoteMenu from '@/components/NoteMenu';
 import NoteViewer from '@/components/UI/NoteViewer';
 import useUserNotes from '@/hooks/useUserNotes';
-import { atomIsMobile } from '@/stores/appStore';
+import { atomIsMobile, atomModal } from '@/stores/appStore';
 import { atomNotesSelected, atomNotesSelectedId } from '@/stores/notesStore';
 import { atomUser } from '@/stores/userStore';
 import { useAtomValue, useSetAtom } from 'jotai';
@@ -13,25 +14,29 @@ const PageHome = () => {
   const user = useAtomValue(atomUser);
   const selectedNote = useAtomValue(atomNotesSelected);
   const setSelectedNoteId = useSetAtom(atomNotesSelectedId);
-  const { deleteNote, updateNote, addNote } = useUserNotes();
+  const { updateNote, addNote } = useUserNotes();
+  const setModal = useSetAtom(atomModal);
 
   if (!user) return <Navigate to="/login" replace />;
 
   return (
-    <main className="fixed inset-0 divide-x divide-slate-700 bg-slate-800 text-lg text-slate-300 md:flex">
-      <NoteMenu />
+    <>
+      <main className="fixed inset-0 divide-x divide-slate-700 bg-slate-800 text-lg text-slate-300 md:flex">
+        <NoteMenu />
 
-      {selectedNote ? (
-        <NoteViewer
-          note={selectedNote}
-          onDeleteNote={() => deleteNote(selectedNote)}
-          onNoteChange={(updated) => updateNote(updated, selectedNote)}
-          onClosed={isMobile ? () => setSelectedNoteId(null) : undefined}
-        />
-      ) : (
-        <FallbackNoSelectedNote onAddNote={addNote} />
-      )}
-    </main>
+        {selectedNote ? (
+          <NoteViewer
+            note={selectedNote}
+            onDeleteNote={() => setModal('delete-note')}
+            onNoteChange={(updated) => updateNote(updated, selectedNote)}
+            onClosed={isMobile ? () => setSelectedNoteId(null) : undefined}
+          />
+        ) : (
+          <FallbackNoSelectedNote onAddNote={addNote} />
+        )}
+      </main>
+      <ContainerModal />
+    </>
   );
 };
 
