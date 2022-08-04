84e2ec1a4707be00321a178a8fc76bca2cbf3493
Royman <royman.dev@gmail.com>
2022-08-05 00:53:14 +0700

    feat(store): appStore add modal atom

--- src/stores/appStore.ts
+++ src/stores/appStore.ts
@@ -1,3 +1,6 @@
 import { atom } from 'jotai';
 
 export const atomIsMobile = atom(false);
+
+export const atomModal = atom<'delete-note' | null>(null);
+export const atomModalClose = atom(null, (get, set) => set(atomModal, null));
